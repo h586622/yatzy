9db502f23485a1db435a167319bfa91ccd9b99fe
Håkon Øygard <150302@stud.hvl.no>
2021-03-25 11:15:53 +0100

    mapping database, lagt til dataSources.xml for å bruke database direkte i IntelliJ

--- src/main/java/no/gruppe6/yatzy/entities/Bruker.java
+++ src/main/java/no/gruppe6/yatzy/entities/Bruker.java
@@ -1,50 +1,93 @@
 package no.gruppe6.yatzy.entities;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(schema = "yatzydb")
 public class Bruker {
-	
-	private String brukernavn;
-	private String epost;
-	private Passord passord;
-	private boolean admin;
-	
-	public Bruker(String brukernavn, String epost, Passord passord) {
-		this.brukernavn = brukernavn;
-		this.epost = epost;
-		this.passord = passord;
-		admin = false;
-	}
-	
-	public Bruker() {
-		
-	}
-	
-	
-	
 
-	public String getBrukernavn() {
-		return brukernavn;
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String brukernavn;
+    private String fornavn;
+    private String etternavn;
+    @Embedded
+    private Passord passord;
+    private String epost;
 
-	public void setBrukernavn(String brukernavn) {
-		this.brukernavn = brukernavn;
-	}
+    @OneToMany(mappedBy = "bruker")
+    private List<Spilldeltagelse> spilldeltagelseList;
 
-	public String getEpost() {
-		return epost;
-	}
+    @OneToMany(mappedBy = "brukerTur")
+    private List<Spill> spillTur;
 
-	public void setEpost(String epost) {
-		this.epost = epost;
-	}
+    public Bruker(String brukernavn, String epost, Passord passord) {
+        this.brukernavn = brukernavn;
+        this.epost = epost;
+        this.passord = passord;
+    }
 
-	public Passord getPassord() {
-		return passord;
-	}
+    public Bruker() {
 
-	public void setPassord(Passord passord) {
-		this.passord = passord;
-	}
-	
-	
+    }
+
+    public String getFornavn() {
+        return fornavn;
+    }
+
+    public void setFornavn(String fornavn) {
+        this.fornavn = fornavn;
+    }
+
+    public String getEtternavn() {
+        return etternavn;
+    }
+
+    public void setEtternavn(String etternavn) {
+        this.etternavn = etternavn;
+    }
+
+    public List<Spilldeltagelse> getSpilldeltagelseList() {
+        return spilldeltagelseList;
+    }
+
+    public void setSpilldeltagelseList(List<Spilldeltagelse> spilldeltagelseList) {
+        this.spilldeltagelseList = spilldeltagelseList;
+    }
+
+    public List<Spill> getSpillTur() {
+        return spillTur;
+    }
+
+    public void setSpillTur(List<Spill> spillTur) {
+        this.spillTur = spillTur;
+    }
+
+    public String getBrukernavn() {
+        return brukernavn;
+    }
+
+    public void setBrukernavn(String brukernavn) {
+        this.brukernavn = brukernavn;
+    }
+
+    public String getEpost() {
+        return epost;
+    }
+
+    public void setEpost(String epost) {
+        this.epost = epost;
+    }
+
+    public Passord getPassord() {
+        return passord;
+    }
+
+    public void setPassord(Passord passord) {
+        this.passord = passord;
+    }
+
 
 }
